2ed39f80160a5a031141206775fd6a431a112322
Thomas Alzate <targam2021@gmail.com>
2024-10-06 13:16:26 -0500

    fully working users page

--- src/app/demo/your-deliveries/your-deliveries.component.ts
+++ src/app/demo/your-deliveries/your-deliveries.component.ts
@@ -3,10 +3,14 @@ import { CardComponent } from 'src/app/theme/shared/components/card/card.compone
 import { MessageComponent } from 'src/app/message/message.component';
 import { FormsModule } from '@angular/forms';
 import { CHAT } from 'src/data/chat.data';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { matLocationOnRound, matCheckRound, matMarkunreadMailboxRound, matMyLocationRound, matPersonRound, matMotorcycleRound, matInfoRound, matArrowForwardRound } from '@ng-icons/material-icons/round'
+
 @Component({
   selector: 'app-your-deliveries',
   standalone: true,
-  imports: [CardComponent, MessageComponent, FormsModule],
+  imports: [CardComponent, MessageComponent, FormsModule, NgIconComponent],
+  viewProviders: [provideIcons({ matLocationOnRound, matCheckRound, matMarkunreadMailboxRound, matMyLocationRound, matPersonRound, matMotorcycleRound, matInfoRound, matArrowForwardRound })],
   templateUrl: './your-deliveries.component.html',
   styleUrl: './your-deliveries.component.scss'
 })
@@ -14,7 +18,7 @@ import { CHAT } from 'src/data/chat.data';
 export class YourDeliveriesComponent {
   chat = CHAT;
   message = "";
-
+  status = 3;
   sendMessage() {
     CHAT.push({
       name: "Thomas",
